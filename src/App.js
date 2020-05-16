d7de8bcb592d7e8df823345d1313ee76d1e9be6a
Franco Martinez Carballo <franco.mcarballo@outlook.com>
2020-05-15 22:01:23 -0600

    Add Alert Component, Search Form, Clear State Function, Parent-Child Communication, onSubmit, onClick, onChange

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import NavBar from './components/layout/Navbar';
+import Alert from './components/layout/Alert';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
 import './App.css';
 
@@ -8,29 +10,69 @@ class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
   async componentDidMount() {
-    this.setState({ loading: true });
-    const response = await axios.get(`https://api.github.com/users?
-    client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
-    client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}
-    `);
+    // this.setState({ loading: true });
+    // const response = await axios.get(`https://api.github.com/users?
+    // client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+    // client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}
+    // `);
 
-    this.setState({ users: response.data, loading: false });
+    // this.setState({ users: response.data, loading: false });
+    this.searchUsers('francomac');
   }
 
+  // Search Github Users
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+
+    const response = await axios.get(
+      `https://api.github.com/search/users?q=${text}&
+    client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+    client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}
+    `
+    );
+
+    this.setState({ users: response.data.items, loading: false });
+  };
+
+  setAlert = (newMsg, newType) => {
+    this.setState({ alert: { msg: newMsg, type: newType } });
+
+    setTimeout(() => {
+      this.setState({ alert: null });
+    }, 3000);
+  };
+
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
+
   render() {
-    const name = 'FrancoMaC';
-    const loading = false;
+    const { users, loading } = this.state;
+    const name = 'francomac';
+    const loadingTitle = false;
     const showName = true;
 
     return (
       <div className='App'>
-        {loading ? <h4>Loading...</h4> : <h3>Hello {showName && name}!</h3>}
         <NavBar title='Github Finder' />
+        {loadingTitle ? (
+          <h4>Loading...</h4>
+        ) : (
+          <h3>Hello {showName && name}!</h3>
+        )}
         <div className='container'>
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Alert alert={this.state.alert} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
